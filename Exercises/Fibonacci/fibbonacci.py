c2ffb29d801b77ac19534d474f109f0c4b18e586
Snabelhack <pettersalenback@gmail.com>
2023-01-08 13:16:34 +0100

    Updated Main program

---
+++ Exercises/Fibonacci/fibbonacci.py
@@ -0,0 +1,27 @@
+
+
+# fibonacci_class.py
+
+class Fibonacci:
+    # Initialize the Fibonacci object
+    def __init__(self):
+        # Cache the two base cases
+        self.cache = [0, 1]
+        
+    def __call__(self, n):
+        # Validate value of n, raise exception if not valid.
+        if not(isinstance(n, int) or n >= 0):
+            raise ValueError(f'Expected postive integer, input was "{n}"')
+            
+        # Check cache for previously calculated numbers
+        if n < len(self.cache):
+            return self.cache[n]
+        
+        # Else compute and append new number in Fib series
+        else: 
+            fib = self(n - 1) + self(n - 2)
+            self.cache.append(fib)
+        return self.cache[n]
+
+
+
---
+++ Exercises/Fibonacci/main.py
@@ -0,0 +1,7 @@
+import fibbonacci as fib
+
+# Main
+print("Skriv in ett heltal: ")
+var = int(input())
+
+print(var)
